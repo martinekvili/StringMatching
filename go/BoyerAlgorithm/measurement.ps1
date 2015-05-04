848585bb348ca3fa1903f4db714e7b3b9a9bf87f
Martinek Vilmos <vilmos.martinek@gmail.com>
2015-05-05 00:11:06 +0200

    Added longer words

--- go/BoyerAlgorithm/measurement.ps1
+++ go/BoyerAlgorithm/measurement.ps1
@@ -4,8 +4,13 @@ Write-Output("Starting measurement of string matching algorithms")
 Write-Output("")
 
 $files = ("bfranklin.txt", "dostoyevsky.txt", "jackrabbit.txt", "napoleon.txt", "dante.txt", "spacewrecked.txt")
-$words = ("contiguity", "hereditary", "knapsack", "expressive", "adversary", "plentifully")
+
+$words = ( "fire mixed with water", "significant object at", "rappers from the rose",
+               "almost immediately be", "ow a corpse they left", "retreat into the bush")
+
+#$words = ("contiguity", "hereditary", "smithereen", "expressive", "adversary", "plentifully")
+
 for ($i = 0; $i -lt 6; $i += 1) {
 	Write-Output("String matching on file $($files[$i]) with word $($words[$i])...")
-	.\BoyerAlgorithm.exe $($files[$i]) $($words[$i]) | Out-File data.csv -Append
+	.\BoyerAlgorithm.exe $($files[$i]) "$($words[$i])" | Out-File data.csv -Append
 	}
