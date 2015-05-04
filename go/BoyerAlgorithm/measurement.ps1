4318169e1bb09c286abfd7e97e45c769527aa585
Martinek Vilmos <vilmos.martinek@gmail.com>
2015-05-04 21:54:32 +0200

    PowerShell intead of batch

---
+++ go/BoyerAlgorithm/measurement.ps1
@@ -0,0 +1,11 @@
+﻿Clear-Content data.csv
+
+Write-Output("Starting measurement of string matching algorithms")
+Write-Output("")
+
+$files = ("bfranklin.txt", "dostoyevsky.txt", "jackrabbit.txt", "napoleon.txt", "dante.txt", "spacewrecked.txt")
+$words = ("contiguity", "hereditary", "knapsack", "expressive", "adversary", "plentifully")
+for ($i = 0; $i -lt 6; $i += 1) {
+	Write-Output("String matching on file $($files[$i]) with word $($words[$i])...")
+	.\BoyerAlgorithm.exe $($files[$i]) $($words[$i]) | Out-File data.csv -Append
+	}
