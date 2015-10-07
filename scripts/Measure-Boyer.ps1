5755ffbb5c10921fa725eb8bffe14b7728fd609d
Martinek Vilmos <vilmos.martinek@gmail.com>
2015-10-07 15:47:27 +0200

    Boyer measuring script upgraded

--- scripts/Measure-Boyer.ps1
+++ scripts/Measure-Boyer.ps1
@@ -10,60 +10,68 @@ $files = ("bfranklin.txt", "dostoyevsky.txt", "jackrabbit.txt", "napoleon.txt", 
 #$words = ( "fire mixed with water", "significant object at", "rappers from the rose",
 #               "almost immediately be", "ow a corpse they left", "retreat into the bush")
 
-$words = ("contiguity", "hereditary", "smithereen", "expressive", "adversary", "plentifully")
-
-Write-Output("Starting measurement of string matching algorithms")
+$words = ("contiguity", "hereditary", "smithereen", "expressive", "forfeiture", "plentifull")
 
 if ($cpp) {
-    Write-Output("`nStarting C++ measurement`n")
-
     if (Test-Path data_cpp.csv) {
         Remove-Item data_cpp.csv
     }
 
     for ($i = 0; $i -lt 6; $i += 1) {
-	    Write-Output("String matching on file $($files[$i]) with word $($words[$i])...")
-	    ..\cpp\BoyerAlgorithm\x64\Release\BoyerAlgorithm.exe $($files[$i]) "$($words[$i])" | Out-File data_cpp.csv -Append
-	    }
+	    Write-Progress -Activity "C++ matching" `
+                       -CurrentOperation "String matching on file $($files[$i]) with word `"$($words[$i])`"" `
+                       -PercentComplete $($i / 6 * 100)
+
+	    ..\cpp\BoyerAlgorithm\x64\Release\BoyerAlgorithm.exe $($files[$i]) "$($words[$i])" | 
+                                                     Out-File data_cpp.csv -Append
+    }
+
+    Write-Progress -Activity "C++ matching" -Completed
 }
 
 if ($cs) {
-    Write-Output("`nStarting C# measurement`n")
-
     if (Test-Path data_cs.csv) {
         Remove-Item data_cs.csv
     }
 
     for ($i = 0; $i -lt 6; $i += 1) {
-	    Write-Output("String matching on file $($files[$i]) with word $($words[$i])...")
-	    ..\cs\BoyerAlgorithm\BoyerAlgorithm\bin\Release\BoyerAlgorithm.exe $($files[$i]) "$($words[$i])" | Out-File data_cs.csv -Append
-	    }
+	    Write-Progress -Activity "C# matching" `
+                       -CurrentOperation "String matching on file $($files[$i]) with word `"$($words[$i])`"" `
+                       -PercentComplete $($i / 6 * 100)
+
+	    ..\cs\BoyerAlgorithm\BoyerAlgorithm\bin\Release\BoyerAlgorithm.exe $($files[$i]) "$($words[$i])" | 
+                                                    Out-File data_cs.csv -Append
+    }
 }
 
 if ($java) {
-    Write-Output("`nStarting Java measurement`n")
-
     if (Test-Path data_java.csv) {
         Remove-Item data_java.csv
     }
 
     for ($i = 0; $i -lt 6; $i += 1) {
-	    Write-Output("String matching on file $($files[$i]) with word $($words[$i])...")
-	    java -cp ..\java\BoyerAlgorithm\bin\ boyer.Program $($files[$i]) "$($words[$i])" | Out-File data_java.csv -Append
-	    }
+	    Write-Progress -Activity "Java matching" `
+                       -CurrentOperation "String matching on file $($files[$i]) with word `"$($words[$i])`"" `
+                       -PercentComplete $($i / 6 * 100)
+
+	    java -cp ..\java\BoyerAlgorithm\bin\ boyer.Program $($files[$i]) "$($words[$i])" | 
+                                                    Out-File data_java.csv -Append
+    }
 }
 
 if ($go) {
-    Write-Output("`nStarting Go measurement`n")
-
     if (Test-Path data_go.csv) {
         Remove-Item data_go.csv
     }
 
     for ($i = 0; $i -lt 6; $i += 1) {
-	    Write-Output("String matching on file $($files[$i]) with word $($words[$i])...")
-	    ..\go\BoyerAlgorithm\BoyerAlgorithm.exe $($files[$i]) "$($words[$i])" | Out-File data_go.csv -Append
+	    Write-Progress -Activity "Go matching" `
+                       -CurrentOperation "String matching on file $($files[$i]) with word `"$($words[$i])`"" `
+                       -PercentComplete $($i / 6 * 100)
+
+	    ..\go\BoyerAlgorithm\BoyerAlgorithm.exe $($files[$i]) "$($words[$i])" | 
+                                                    Out-File data_go.csv -Append
 	    }
 }
 
-Write-Output("`nAll measurements DONE.")
+Write-Host -ForegroundColor Green "All measurements DONE."
