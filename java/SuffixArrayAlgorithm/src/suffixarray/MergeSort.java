9d10d2743f0a32e8860fd9a9c8165f3cb4c74366
Martinek Vilmos <vilmos.martinek@gmail.com>
2015-10-23 12:06:03 +0200

    Java SuffixArray started

---
+++ java/SuffixArrayAlgorithm/src/suffixarray/MergeSort.java
@@ -0,0 +1,146 @@
+package suffixarray;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class MergeSort {
+	public class Comparer implements Comparator<Integer> {
+		private String str;
+
+		public Comparer(String s) {
+			str = s;
+		}
+
+		@Override
+		public int compare(Integer o1, Integer o2) {
+			// return String.compare(str, x, str, y, str.length());
+			return 0;
+		}
+	}
+
+	private static final int mergeTreshold = 8192;
+	private static final int sortTreshold = 48;
+
+	private static void sequentialMerge(Integer[] arr1, int start1, int end1, Integer[] arr2, int start2, int end2,
+			Integer[] dst, int start_dst, Comparator<Integer> comp) {
+		while (start1 != end1 && start2 != end2) {
+			if (comp.compare(arr1[start1], arr2[start2]) < 0) {
+				dst[start_dst] = arr1[start1];
+				start1++;
+			} else {
+				dst[start_dst] = arr2[start2];
+				start2++;
+			}
+			start_dst++;
+		}
+
+		while (start1 != end1) {
+			dst[start_dst] = arr1[start1];
+			start1++;
+			start_dst++;
+		}
+
+		while (start2 != end2) {
+			dst[start_dst] = arr2[start2];
+			start2++;
+			start_dst++;
+		}
+	}
+
+	private static void parallelMerge(Integer[] arr, int start1, int end1, int start2, int end2, Integer[] dst, int pos,
+			Comparator<Integer> comparer, ExecutorService executor) {
+		int length1 = end1 - start1;
+		int length2 = end2 - start2;
+
+		if (length1 == 0 && length2 == 0) {
+			return;
+		}
+
+		if ((length1 + length2) < mergeTreshold) {
+			sequentialMerge(arr, start1, end1, arr, start2, end2, dst, pos, comparer);
+		} else {
+			int xpos1 = (start1 + end1) / 2;
+
+			int xpos2 = Arrays.binarySearch(arr, start2, end2 - start2, arr[xpos1], comparer);
+			xpos2 = xpos2 < 0 ? -(xpos2 + 1) : xpos2; // A dokumentáció szerint
+														// így kapjuk meg az
+														// elsõ nem kisebb elem
+														// indexét.
+
+			int xpos_dst = pos + (xpos1 - start1) + (xpos2 - start2);
+
+			dst[xpos_dst] = arr[xpos1];
+
+			final int xpos2_f = xpos2; // Csak hogy használhassuk enclosing
+										// scope-on belül
+
+			List<Callable<Object>> tasks = Arrays.asList(Executors.callable(() -> {
+				parallelMerge(arr, start1, xpos1, start2, xpos2_f, dst, pos, comparer, executor);
+			}), Executors.callable(() -> {
+				parallelMerge(arr, xpos1 + 1, end1, xpos2_f, end2, dst, xpos_dst + 1, comparer, executor);
+			}));
+
+			try {
+				executor.invokeAll(tasks);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+
+	static void parallelSort(Integer[] src, int start, int end, Integer[] dest, Comparator<Integer> comparer,
+			boolean srcToDest, ExecutorService executor) {
+		if (start == end) {
+			return;
+		}
+
+		if ((end - start) < sortTreshold) {
+			Arrays.sort(src, start, end - start, comparer);
+
+			if (srcToDest) {
+				for (int i = start; i < end; i++) {
+					dest[i] = src[i];
+				}
+			}
+
+			return;
+		}
+
+		int middle = (start + end) / 2;
+
+		List<Callable<Object>> tasks = Arrays.asList(Executors.callable(() -> {
+			parallelSort(src, start, middle, dest, comparer, !srcToDest, executor);
+		}), Executors.callable(() -> {
+			parallelSort(src, middle, end, dest, comparer, !srcToDest, executor);
+		}));
+
+		try {
+			executor.invokeAll(tasks);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		if (srcToDest) {
+			parallelMerge(src, start, middle, middle, end, dest, start, comparer, executor);
+		} else {
+			parallelMerge(dest, start, middle, middle, end, src, start, comparer, executor);
+		}
+	}
+
+	public static void ParallelSort(Integer[] arr, Comparator<Integer> comparer) {
+		Integer[] acc = new Integer[arr.length];
+
+		ExecutorService executor = Executors.newWorkStealingPool();
+
+		parallelSort(arr, 0, arr.length, acc, comparer, false, executor);
+
+		executor.shutdown();
+	}
+
+}
