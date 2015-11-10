8daf4cb90af1d74a35df80bbe2b3bfab74f5f5d9
Martinek Vilmos <vilmos.martinek@gmail.com>
2015-11-10 14:14:42 +0100

    Obsoloete Go projects removed

--- go/BoyerAlgorithm/make.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-go build -o BoyerAlgorithm.exe program.go pattern.go match.go
-
-if %errorlevel% equ 0 gofmt -w program.go pattern.go match.go
--- go/SuffixArrayAlgorithm/make.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-go build -o SuffixArrayAlgorithm.exe program.go mergesort.go comparers.go matchablestring.go
-
-if %errorlevel% equ 0 gofmt -w program.go mergesort.go comparers.go matchablestring.go
