68c9ce544f617af27f97683ac4c3527468854372
Martinek Vilmos <vilmos.martinek@gmail.com>
2015-11-12 01:32:15 +0100

    Added new measuring script

---
+++ scripts/Measure-Both.ps1
@@ -0,0 +1,85 @@
+﻿Param(
+    [switch] $cpp,
+    [switch] $cs,
+    [switch] $java,
+    [switch] $go)
+
+$numbers = @(1, 10, 50, 100, 200, 500, 750, 1000, 1500, 2000)
+
+$resources = Import-Csv -Delimiter ";" -Path ..\resources.csv
+
+if ($cpp) {
+    if (Test-Path Both\data_cpp.csv) {
+        Remove-Item Both\data_cpp.csv
+    }
+
+    $i = 0
+    foreach ($number in $numbers) {
+	    Write-Progress -Activity "C++ matching" `
+                       -CurrentOperation "String matching on file $($resources[4].FileName) with word `"$($resources[4].Word)`" for $($number) times" `
+                       -PercentComplete $($i / 10 * 100)
+
+	    ..\cpp\MatchableString\x64\Release\MatchableString.exe both $($resources[4].FileName) "$($resources[4].Word)" 1 $number | 
+                                                     Out-File Both\data_cpp.csv -Append
+        
+        $i += 1
+    }
+
+    Write-Progress -Activity "C++ matching" -Completed
+}
+
+if ($cs) {
+    if (Test-Path Both\data_cs.csv) {
+        Remove-Item Both\data_cs.csv
+    }
+
+    $i = 0
+    foreach ($number in $numbers) {
+	    Write-Progress -Activity "C# matching" `
+                       -CurrentOperation "String matching on file $($resources[4].FileName) with word `"$($resources[4].Word)`" for $($number) times" `
+                       -PercentComplete $($i / 10 * 100)
+
+	    ..\cs\MatchableString\MatchableString\bin\Release\MatchableString.exe both $($resources[4].FileName) "$($resources[4].Word)" 1 $number | 
+                                                    Out-File Both\data_cs.csv -Append
+
+        $i += 1
+    }
+}
+
+if ($java) {
+    if (Test-Path Both\data_java.csv) {
+        Remove-Item Both\data_java.csv
+    }
+
+    $i = 0
+    foreach ($number in $numbers) {
+	    Write-Progress -Activity "Java matching" `
+                       -CurrentOperation "String matching on file $($resources[4].FileName) with word `"$($resources[4].Word)`" for $($number) times" `
+                       -PercentComplete $($i / 10 * 100)
+
+	    java -cp ..\java\MatchableString\out\production\MatchableString matchablestring.Program both $($resources[4].FileName) "$($resources[4].Word)" 1 $number | 
+                                                    Out-File Both\data_java.csv -Append
+        
+        $i += 1
+    }
+}
+
+if ($go) {
+    if (Test-Path Both\data_go.csv) {
+        Remove-Item Both\data_go.csv
+    }
+
+    $i = 0
+    foreach ($number in $numbers) {
+	    Write-Progress -Activity "Go matching" `
+                       -CurrentOperation "String matching on file $($resources[4].FileName) with word `"$($resources[4].Word)`" for $($number) times" `
+                       -PercentComplete $($i / 10 * 100)
+
+	    ..\go\MatchableString\MatchableString.exe both $($resources[4].FileName) "$($resources[4].Word)" 1 $number | 
+                                                    Out-File Both\data_go.csv -Append
+        
+        $i += 1
+    }
+}
+
+Write-Host -ForegroundColor Green "All measurements DONE."
