add7b6933a25bdcdd8007ec3e4fc29883c3d7f51
Martinek Vilmos <vilmos.martinek@gmail.com>
2015-11-18 17:29:36 +0100

    Minor changes in Java AverageCounter

--- java/MatchableString/src/matchablestring/AverageCounter.java
+++ java/MatchableString/src/matchablestring/AverageCounter.java
@@ -9,19 +9,11 @@ public class AverageCounter {
     public static long countAverage(List<Long> data) {
         Collections.sort(data);
 
-//        System.out.println("\nData is:");
-//        for (Long l : data) {
-//            System.out.println(l);
-//        }
-
         long Q1 = data.get(0);
         long Q2 = data.get(data.size() / 2);
-        long diff = (long) ((Q2 - Q1) * 1.5);
-
-//        System.out.println("Q1: " + Q1 + ", Q2: " + Q2 + ", diff: " + diff);
+        long diff = (Q2 - Q1) * 3;
 
         List<Long> filtered = data.stream().filter(d -> d <= Q2 + diff).collect(Collectors.toList());
-
         return filtered.stream().reduce(0L, (acc, d) -> acc + d) / filtered.size();
     }
 }
